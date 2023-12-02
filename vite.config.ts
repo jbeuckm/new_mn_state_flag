0459a44f82f8d80fb90734a460ec04cb7e152e6f
Joe Beuckman <jbeuckman@lm-jbeuckman.home>
2023-12-01 20:00:14 -0600

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/new_mn_state_flag/",
   plugins: [react()],
   build: {
     outDir: "./docs",
