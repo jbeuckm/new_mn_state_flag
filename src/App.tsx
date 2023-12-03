d532de3d3f229e5dcf0334feb2c830016ee296d6
Joe Beuckman <jbeuckman@lm-jbeuckman.home>
2023-12-03 08:18:37 -0600

    extract design

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { Box, Button, Stack } from "@mui/material";
 import { useEffect, useState } from "react";
 import { ChromePicker } from "react-color";
 import * as htmlToImage from "html-to-image";
+import { F1953 } from "./designs/F1953";
 
 function App() {
-  const [hoistColor, setHoistColor] = useState("#153573");
-  const [starColor, setStarColor] = useState("#EBDF09");
-  const [stateColor, setStateColor] = useState("#009BEF");
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  console.log(urlParams.get("d"));
+
+  const [color1, setColor1] = useState("#153573");
+  const [color2, setColor2] = useState("#EBDF09");
+  const [color3, setColor3] = useState("#009BEF");
 
   useEffect(() => {
     var node = document.getElementById("flag");
@@ -17,84 +22,39 @@ function App() {
       // @ts-ignore - it's there because the html has flagWaver code
       window.flagWaver.flag.setOpts({ imgSrc: dataUrl });
     });
-  }, [hoistColor, starColor, stateColor]);
+  }, [color1, color2, color3]);
 
   return (
     <Stack>
       <div id="flag">
-        <svg width="500" height="300" xmlns="http://www.w3.org/2000/svg">
-          <rect width="300" height="300" fill={hoistColor} />
-
-          <path
-            d="M 0 0 L 200 0 L 100 150 L 200 300 L 0 300"
-            fill={stateColor}
-            transform="translate(300)"
-          />
-
-          <g transform="translate(150 150) scale(50 50)" fill={starColor}>
-            <g transform="rotate(45)">
-              <path
-                d="M 0 0 L 1 0 L 1.707 .707 L .707 .707"
-                transform="translate(.24 .1)"
-              />
-              <path
-                d="M 0 0 L 1 0 L 1.707 -.707 L .707 -.707"
-                transform="translate(.24 -.1)"
-              />
-            </g>
-            <g transform="rotate(135)">
-              <path
-                d="M 0 0 L 1 0 L 1.707 .707 L .707 .707"
-                transform="translate(.24 .1)"
-              />
-              <path
-                d="M 0 0 L 1 0 L 1.707 -.707 L .707 -.707"
-                transform="translate(.24 -.1)"
-              />
-            </g>
-            <g transform="rotate(-45)">
-              <path
-                d="M 0 0 L 1 0 L 1.707 .707 L .707 .707"
-                transform="translate(.24 .1)"
-              />
-              <path
-                d="M 0 0 L 1 0 L 1.707 -.707 L .707 -.707"
-                transform="translate(.24 -.1)"
-              />
-            </g>
-            <g transform="rotate(-135)">
-              <path
-                d="M 0 0 L 1 0 L 1.707 .707 L .707 .707"
-                transform="translate(.24 .1)"
-              />
-              <path
-                d="M 0 0 L 1 0 L 1.707 -.707 L .707 -.707"
-                transform="translate(.24 -.1)"
-              />
-            </g>
-            <use href="#quarter" transform="rotate(135)" />
-            <use href="#quarter" transform="rotate(-135)" />
-            <use href="#quarter" transform="rotate(45)" />
-          </g>
-        </svg>
+        <F1953
+          color1={color1}
+          color2={color2}
+          color3={color3}
+          setDefaultColors={({ color1, color2, color3 }) => {
+            setColor1(color1);
+            setColor2(color2);
+            setColor3(color3);
+          }}
+        />
       </div>
 
       <Box style={{ backgroundColor: "#ffffff" }}>
         <Stack direction="row">
           <ChromePicker
             disableAlpha
-            color={starColor}
-            onChange={({ hex }) => setStarColor(hex)}
+            color={color2}
+            onChange={({ hex }) => setColor2(hex)}
           />
           <ChromePicker
             disableAlpha
-            color={hoistColor}
-            onChange={({ hex }) => setHoistColor(hex)}
+            color={color1}
+            onChange={({ hex }) => setColor1(hex)}
           />
           <ChromePicker
             disableAlpha
-            color={stateColor}
-            onChange={({ hex }) => setStateColor(hex)}
+            color={color3}
+            onChange={({ hex }) => setColor3(hex)}
           />
         </Stack>
       </Box>
---
+++ src/designs/F1953.tsx
@@ -0,0 +1,70 @@
+import { useEffect } from "react";
+
+export const F1953 = ({ color1, color2, color3, setDefaultColors }: any) => {
+  useEffect(() => {
+    setDefaultColors &&
+      setDefaultColors({
+        color1: "#153573",
+        color2: "#EBDF09",
+        color3: "#009BEF",
+      });
+  }, []);
+
+  return (
+    <svg width="500" height="300" xmlns="http://www.w3.org/2000/svg">
+      <rect width="300" height="300" fill={color1} />
+
+      <path
+        d="M 0 0 L 200 0 L 100 150 L 200 300 L 0 300"
+        fill={color3}
+        transform="translate(300)"
+      />
+
+      <g transform="translate(150 150) scale(50 50)" fill={color2}>
+        <g transform="rotate(45)">
+          <path
+            d="M 0 0 L 1 0 L 1.707 .707 L .707 .707"
+            transform="translate(.24 .1)"
+          />
+          <path
+            d="M 0 0 L 1 0 L 1.707 -.707 L .707 -.707"
+            transform="translate(.24 -.1)"
+          />
+        </g>
+        <g transform="rotate(135)">
+          <path
+            d="M 0 0 L 1 0 L 1.707 .707 L .707 .707"
+            transform="translate(.24 .1)"
+          />
+          <path
+            d="M 0 0 L 1 0 L 1.707 -.707 L .707 -.707"
+            transform="translate(.24 -.1)"
+          />
+        </g>
+        <g transform="rotate(-45)">
+          <path
+            d="M 0 0 L 1 0 L 1.707 .707 L .707 .707"
+            transform="translate(.24 .1)"
+          />
+          <path
+            d="M 0 0 L 1 0 L 1.707 -.707 L .707 -.707"
+            transform="translate(.24 -.1)"
+          />
+        </g>
+        <g transform="rotate(-135)">
+          <path
+            d="M 0 0 L 1 0 L 1.707 .707 L .707 .707"
+            transform="translate(.24 .1)"
+          />
+          <path
+            d="M 0 0 L 1 0 L 1.707 -.707 L .707 -.707"
+            transform="translate(.24 -.1)"
+          />
+        </g>
+        <use href="#quarter" transform="rotate(135)" />
+        <use href="#quarter" transform="rotate(-135)" />
+        <use href="#quarter" transform="rotate(45)" />
+      </g>
+    </svg>
+  );
+};
